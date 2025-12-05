c4103c638257c96f3141052485e4d771ef3f308f
avizaguri-macbook <avi210892@gmail.com>
2025-12-05 09:06:09 +0200

    Remove unused pytest-depends-on plugin configuration from conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,4 @@
 from custom_python_logger import build_logger
 
-# pytest_plugins = [
-#     "pytest_depends_on.depends_on",
-# ]
 
 logger = build_logger(project_name="pytest-depends-on", log_file=True)
