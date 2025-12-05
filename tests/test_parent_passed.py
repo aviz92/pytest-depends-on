fae298152eee4282f5d124eed0bbeb3a98c1dc0e
avizaguri-macbook <avi210892@gmail.com>
2025-12-05 08:25:41 +0200

    Refactor test functions and add type hints for improved clarity

--- tests/test_parent_passed.py
+++ tests/test_parent_passed.py
@@ -3,15 +3,15 @@ import pytest
 from pytest_depends_on.consts.status import Status
 
 
-def test_parent():
+def test_parent() -> None:
     assert True
 
 
 @pytest.mark.depends_on(tests=["test_parent"], status=Status.PASSED)
-def test_child_a():
+def test_child_a() -> None:
     assert True
 
 
 @pytest.mark.depends_on(tests=["test_parent"], status=Status.FAILED)
-def test_child_b():  # expected to status "SKIPPED"
+def test_child_b() -> None:  # expected to status "SKIPPED"
     assert True
